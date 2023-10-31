7f79d3d8b060afda9095c73f8b8b858ee460183d
Hrishi00007 <109570954+Hrishi00007@users.noreply.github.com>
2023-10-31 11:39:22 +0530

    Delete ROSP Project directory

--- ROSP Project/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "admin123",
-    authDomain: "firebase",
-    projectId: "1",
-    storageBucket: "bucket1",
-    messagingSenderId: "root123",
-    appId: "app1"
-  };
-  
