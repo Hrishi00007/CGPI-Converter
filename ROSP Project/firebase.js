2dad4fa9869f84bca41705bb7bdde388bbae9578
Hrishi00007 <109570954+Hrishi00007@users.noreply.github.com>
2023-10-31 11:36:09 +0530

    Added code files

---
+++ ROSP Project/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "admin123",
+    authDomain: "firebase",
+    projectId: "1",
+    storageBucket: "bucket1",
+    messagingSenderId: "root123",
+    appId: "app1"
+  };
+  
---
+++ ROSP Project/sw.js
@@ -0,0 +1,32 @@
+// sw.js
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+  
